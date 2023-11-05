1320126d6ca37178d87dbce2bf70c58d3bae0cb1
faraday22 <faradaysyt@gmail.com>
2023-11-04 22:12:03 -0500

    Neovim go brrrr

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dotfiles
-My dotfiles
+My dotfiles so if I mess up anything which I probably will,
+will be saved for the future and if I get a new computer.
---
+++ configs/nvim/after/plugin/lsp.lua
@@ -0,0 +1,18 @@
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup()
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {
+	  'tsserver', 'clangd', 'bashls', 'cssls',
+	  'html', 'intelephense', 'lua_ls',
+	  'rust_analyzer', 'vimls',},
+  handlers = {
+    lsp_zero.default_setup,
+  }
+})
