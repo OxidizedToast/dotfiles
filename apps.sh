e05d78bbce620aa4f42df0bdfc4a318e50cda75f
faraday22 <faradaysyt@gmail.com>
2023-12-26 20:30:40 -0600

    Updated helpful & bashrc

--- apps.sh
+++ apps.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 #
+#
+#vlc
+#yay
+#Thunar
+#pulseaudio
+#neovim
+#npm
+#lua
+#discord
+#fzf
+#git
+#grim
+#slurp
+#puavcontrol 
+#
+#
+
 sudo pacman -S vlc yay Thunar pulseaudio neovim npm lua discord fzf git grim slurp puavcontrol
