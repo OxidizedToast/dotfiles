171c1e31d7e411c0a7f50097277c8e299e0d2348
faraday22 <faraday2122@protonmail.com>
2024-06-05 21:47:00 -0500

    Waybar Icon, Zsh alias' fixed McShots script

--- helpful/getMcShots.sh
+++ helpful/getMcShots.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-mv ~/.minecraft/screenshots/* ~/ScreenShots
+
+clear
+DIR="~/ScreenShots"
+
+if [ ! -d "$DIR" ]; then
+  mkdir "$DIR"
+  mv ~/.minecraft/screenshots/* ~/ScreenShots/
+  echo "Directory '~/ScreenShots' created!"
+fi
+
+mv ~/.minecraft/screenshots/* ~/ScreenShots/
 echo "Moved Imgs to screenshots"
