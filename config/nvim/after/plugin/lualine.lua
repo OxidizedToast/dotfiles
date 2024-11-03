5f06f64f5875f481253e85a4a17bfd56c563b1ba
OxidizedToast <faraday2122@protonmail.com>
2024-11-03 14:57:04 -0600

    updates

--- config/nvim/after/plugin/lualine.lua
+++ config/nvim/after/plugin/lualine.lua
@@ -1,7 +1,7 @@
 vim.cmd[[ set guifont=Symbols\ devicons\ Font\ 12 ]]
 
 require('lualine').setup {
-  options = { theme = 'horizon', icons_enabled = true },
+  options = { theme = 'nightfly', icons_enabled = true },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
