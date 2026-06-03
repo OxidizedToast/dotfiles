0542051f8bcaf64fe6ae031ce4ef52bdbf3d3260
OxidizedToast <faraday2122@protonmail.com>
2026-06-02 23:10:08 -0500

    Updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,3 +8,11 @@ require("config.remap")
 vim.wo.relativenumber = true
 vim.wo.number = true
 vim.opt.termguicolors = true
+
+-- tab indentation
+vim.opt.tabstop = 4        -- how many spaces a tab looks like
+vim.opt.shiftwidth = 4     -- how many spaces auto-indent uses
+vim.opt.softtabstop = 4    -- how many spaces <Tab> inserts in insert mode
+vim.opt.expandtab = true   -- convert tabs to spaces
+
+
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   lazy = false,
   build = ':TSUpdate',
-  ensure_unstalled = {"c", "lua", "python", "cpp", "bash", "rust", "yaml", "toml", "json", "zig"},
+  ensure_unstalled = {"c", "lua", "python", "cpp", "bash", "rust", "yaml", "toml", "json", "zig", "kotlin"},
   highlight = {
 	enable = true,
 	additional_vim_regex_highlighting = false
